b9a36d63c923f64bd1b0d5f48d44dc7e0d609768
Idir <fouquetidir@gmail.com>
2022-10-09 19:15:27 +0200

    JS - Gestion du submenu secondary

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
 
 
-const icons = document.getElementById("icons");
-const submenu = document.querySelector(".burger-menu-submenu");
+const icons = document.getElementById("icons")
+const submenu = document.querySelector(".burger-menu-submenu")
+const subMenuSecondary = document.querySelector(".burger-menu-submenu-secondary")
+const connect = document.getElementById("connect")
+const connectImage = document.getElementById("connect-image")
 
-icons.addEventListener("click", openMenu);
+icons.addEventListener("click", openMenu)
+connect.addEventListener("click", openSubMenuSecondary)
+
 
 function openMenu () {
      if (icons.src.match("images/icon-hamburger.svg")) {
@@ -18,9 +23,21 @@ function openMenu () {
           icons.src = "images/icon-hamburger.svg"
           submenu.style.display = "none"
           icons.classList.toggle("animations")
+          submenu.classList.remove("submenu-secondary-on")
+          subMenuSecondary.classList.remove("active")
+          connectImage.classList.remove("arrow-active")
+
 
 
      }
+}
+
+function openSubMenuSecondary () {
+     submenu.classList.toggle("submenu-secondary-on")
+     subMenuSecondary.classList.toggle("active")
+     subMenuSecondary.classList.toggle("animations-down")
+     connectImage.classList.toggle("arrow-active")
+
 }
 
 
