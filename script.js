c0045b6847f5e522827d1a1de2b9feb4a0f79cbc
Idir <fouquetidir@gmail.com>
2022-10-11 09:29:11 +0200

    JS - Finalisation

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
 
+// Scroll related animations
 
+const ImageSection1Mobile = document.querySelector(".image-section-1-mobile")
+const ImageSection1Desktop = document.querySelector(".image-section-1-desktop")
+const ImageSection2 = document.getElementById("image-section-2")
+const ImageSection3Mobile = document.querySelector(".image-section-3-mobile")
+const ImageSection3Desktop = document.querySelector(".image-section-3-desktop")
+
+window.addEventListener("scroll", function () {
+
+     const {scrollTop, clientHeight} =
+     document.documentElement;
+
+     const topElementViewport =
+     ImageSection1Mobile.getBoundingClientRect().top
+
+     const topElementViewportbis =
+     ImageSection1Desktop.getBoundingClientRect().top
+
+     const topElementViewport2 =
+     ImageSection2.getBoundingClientRect().top
+
+      const topElementViewport3 =
+     ImageSection3Mobile.getBoundingClientRect().top
+
+     const topElementViewport3bis =
+     ImageSection3Desktop.getBoundingClientRect().top
+
+     if (scrollTop > (scrollTop + topElementViewport).toFixed() - clientHeight ) {
+          ImageSection1Mobile.classList.add("fadeIn")
+     }
+
+     if (scrollTop > (scrollTop + topElementViewportbis).toFixed() - clientHeight * 0.8 ) {
+          ImageSection1Desktop.classList.add("fadeIn")
+     }
+
+
+     if (scrollTop > (scrollTop + topElementViewport2).toFixed() - clientHeight ) {
+          ImageSection2.classList.add("fadeIn")
+
+
+     }
+
+     if (scrollTop > (scrollTop + topElementViewport3bis).toFixed() - clientHeight * 0.8 ) {
+          ImageSection3Desktop.classList.add("fadeIn")
+
+
+     }
+
+     if (scrollTop > (scrollTop + topElementViewport3).toFixed() - clientHeight ) {
+          ImageSection3Mobile.classList.add("fadeIn")
+
+     }
+
+})
+
+
+// Burger menu animations
 
 const icons = document.getElementById("icons")
-const submenu = document.querySelector(".burger-menu-submenu")
-const subMenuSecondary = document.querySelector(".burger-menu-submenu-secondary")
-const connect = document.getElementById("connect")
-const connectImage = document.getElementById("connect-image")
 
 icons.addEventListener("click", openMenu)
-connect.addEventListener("click", openSubMenuSecondary)
-
 
 function openMenu () {
      if (icons.src.match("images/icon-hamburger.svg")) {
@@ -32,6 +83,15 @@ function openMenu () {
      }
 }
 
+// Burger menu - Secondary animations
+
+const submenu = document.querySelector(".burger-menu-submenu")
+const subMenuSecondary = document.querySelector(".burger-menu-submenu-secondary")
+const connect = document.getElementById("connect")
+const connectImage = document.getElementById("connect-image")
+
+connect.addEventListener("click", openSubMenuSecondary)
+
 function openSubMenuSecondary () {
      submenu.classList.toggle("submenu-secondary-on")
      subMenuSecondary.classList.toggle("active")
@@ -39,6 +99,8 @@ function openSubMenuSecondary () {
      connectImage.classList.toggle("arrow-active")
 
 }
+
+
 
 
 
